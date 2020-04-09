0a9758f80f7a369381a8e6fb25f707d4e1a00848
Ernesto <ernesto.exposito@gmail.com>
2020-04-10 00:32:21 +0200

    EIP file integration design pattern based on a shared Database

--- src/eip/fileintegration/db/Exporter.java
+++ src/eip/fileintegration/db/Exporter.java
@@ -20,51 +20,34 @@ import java.util.logging.Logger;
 public class Exporter {
 
     // Java Database Connectivity (JDBC) driver for JavaDB (Derby)   
-    static final String JDBC_DRIVER = "org.apache.derby.jdbc.ClientDriver";  
+    
     // The URL to the database (can be retrieved from netbeans/services/Databases
-    static final String DB_URL = "jdbc:derby://localhost:1527/DATABASE_TEST";
+   
     // the database user
-    static final String USER = "TEST";
+    
     // the databased password
-    static final String PASS = "TEST";
+    
     // the connection to the database to be used to export data 
-    private Connection conn = null;
+    
     
 
     public Exporter() throws Exception {
         // the connection is established to the database
-        Class.forName(JDBC_DRIVER);
-         conn = DriverManager.getConnection(DB_URL,USER,PASS);
+        
     }
 
     public void exportObject(Product p)  {
-        try {
-            // a SQL statement to be sent via the database connection is created 
-            Statement stmt = conn.createStatement();
-            // the SQL string statement is prepared including the product attributes
-            String sql = "INSERT INTO PRODUCT (ID, DESCRIPTION, PRICE, AMOUNT) " +
-                   "VALUES ("
-                    +p.getProductId()+",'"
-                    +p.getProductDescription()+"',"+
-                    +p.getProductPrice()+","+
-                    +p.getProductAmount()+")";
-            // the statement is executed
-            stmt.executeUpdate(sql);
-            // the statmeent is closed. 
-            stmt.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(Exporter.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        // a SQL statement to be sent via the database connection is created
+        // the SQL string statement is prepared including the product attributes
+        // the statement is executed
+        // the statmeent is closed.
+
     }
     
     public void close()  
     {
-        try {
-            // the connection is closed. 
-            conn.close();
-        } catch (SQLException ex) {
-            System.out.println("Error closing the DB");
-        }
+        // the connection is closed.
+
     }
 
 }
